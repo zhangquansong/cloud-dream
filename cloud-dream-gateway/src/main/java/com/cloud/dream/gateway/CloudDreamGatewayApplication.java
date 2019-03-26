8861ace3b7584ba0a7cf6b0aa8e60b19fe01fad5
zhangquansong <zhangquansongjava@163.com>
2019-03-26 11:43:05 +0800

    gateway提交

--- cloud-dream-gateway/src/main/java/com/cloud/dream/gateway/CloudDreamGatewayApplication.java
+++ cloud-dream-gateway/src/main/java/com/cloud/dream/gateway/CloudDreamGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.cloud.dream.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,21 +8,28 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 @EnableEurekaClient
 @RefreshScope
 @EnableDiscoveryClient
 @EnableHystrix
-//@EnableFeignClients
-//@EnableHystrixDashboard
+@EnableFeignClients
+@EnableHystrixDashboard
 public class CloudDreamGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudDreamGatewayApplication.class, args);
     }
+
+    @Value("${cloudDream}")
+    String cloudDream;//springcloud-config-client从git中获取
 
     @Bean
     @LoadBalanced
