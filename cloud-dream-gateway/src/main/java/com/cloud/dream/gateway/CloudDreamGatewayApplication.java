98b54980fc4f7facde1ae8546adc06a6a2984aaa
zhangquansong <zhangquansongjava@163.com>
2019-04-04 19:48:35 +0800

    eureka形式自动匹配路由

--- cloud-dream-gateway/src/main/java/com/cloud/dream/gateway/CloudDreamGatewayApplication.java
+++ cloud-dream-gateway/src/main/java/com/cloud/dream/gateway/CloudDreamGatewayApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
